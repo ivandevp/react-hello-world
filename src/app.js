0866a1ec595f29126191b125fa352fd072b8da31
ivandevp <medinadiazivan@gmail.com>
2016-12-13 14:35:48 -0500

    State management

--- src/app.js
+++ src/app.js
@@ -8,9 +8,21 @@
  import ReactDOM from 'react-dom';
 
  class FirstComponent extends React.Component {
+ 	constructor(props) {
+ 		super(props);
+ 		this.state = { name: '...' };
+ 	}
+ 	changeName(e) {
+ 		let name = e.target.value;
+ 		this.setState({ name: name });
+ 	}
  	render() {
- 		return <h1>Hello {this.props.name}!</h1>
+ 		return <div>
+ 			<label>Enter your name:</label>
+ 			<input type="text" className="username" onChange={this.changeName.bind(this)}/>
+ 			<h1>Hello {this.state.name}!</h1>
+ 		</div>
  	}
  }
 
- ReactDOM.render(<FirstComponent name="Iván"/>, document.getElementById('container'));
+ ReactDOM.render(<FirstComponent />, document.getElementById('container'));
