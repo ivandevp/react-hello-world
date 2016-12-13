cc3f5c7c08b6562fe6134f7777194bcdac285499
astephannie <astephannie@gmail.com>
2016-12-13 12:50:20 -0500

    First component

---
+++ src/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+ * Module dependencies
+ */
+
+ import React from 'react';
+ import ReactDOM from 'react-dom';
+
+ class FirstComponent extends React.Component {
+ 	render() {
+ 		return <h1>Hello world!</h1>
+ 	}
+ }
+
+ ReactDOM.render(<FirstComponent />, document.getElementById('container'));
